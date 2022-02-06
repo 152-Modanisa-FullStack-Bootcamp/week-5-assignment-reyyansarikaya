74c7e2cea7ff52824ddebcdf1e7d0135d17a8bae
F. Reyyan Sarıkaya <reyyansarikaya@gmail.com>
2022-02-06 23:19:25 +0300

    Assignment completed.

--- assignment/assignment.go
+++ assignment/assignment.go
@@ -1,25 +1,103 @@
 package assignment
 
+import (
+	"math"
+	"regexp"
+	"sort"
+	"strings"
+)
+
 func AddUint32(x, y uint32) (uint32, bool) {
-	return 0, false
+	sum := uint64(x) + uint64(y)
+	overflow := uint32(sum>>32) == 1
+	return x + y, overflow
 }
 
 func CeilNumber(f float64) float64 {
-	return 0
+	number, frac := math.Modf(f)
+	if 0 == frac {
+		return f
+	}
+	if 0 < frac && frac <= 0.25 {
+		return number + 0.25
+	}
+
+	if 0.25 < frac && frac <= 0.50 {
+		return number + 0.50
+	}
+
+	if 0.50 < frac && frac <= 0.75 {
+		return number + 0.75
+	}
+	if 0.75 < frac && float32(frac) <= 0.99 {
+		return math.Ceil(f)
+	}
+	return f
 }
 
 func AlphabetSoup(s string) string {
-	return ""
+	stringArray := strings.Split(s, "")
+	sort.Strings(stringArray)
+	justString := strings.Join(stringArray, "")
+	return justString
 }
 
 func StringMask(s string, n uint) string {
-	return ""
+	stringArray := strings.Split(s, "")
+	if len(stringArray) <= 1 {
+		return "*"
+	}
+	if int(n) >= len(stringArray) {
+		n = 0
+	}
+	for j := int(n); j < len(stringArray); j++ {
+		stringArray[j] = "*"
+	}
+	justString := strings.Join(stringArray, "")
+	return justString
 }
 
 func WordSplit(arr [2]string) string {
-	return ""
+	index := 0
+	word := ""
+	matchedWord := ""
+	wordsArray := strings.Split(arr[1], ",")
+	wordArray := strings.Split(arr[0], "")
+out:
+	for i := 0; i < len(wordArray); i++ {
+		for j := 0; j < len(wordsArray); j++ {
+			if index < len([]rune(arr[0])) {
+				word += wordArray[index]
+				index++
+			}
+			matched, _ := regexp.MatchString(wordsArray[j], word)
+			if matched && word == wordsArray[j] {
+				matchedWord += word
+				word = ""
+				if len([]rune(arr[0]))+1 != len([]rune(matchedWord)) {
+					matchedWord += ","
+				} else {
+					break out
+				}
+			}
+		}
+	}
+	if len([]rune(arr[0]))+1 == len([]rune(matchedWord)) {
+		return matchedWord
+	}
+	return "not possible"
 }
 
 func VariadicSet(i ...interface{}) []interface{} {
-	return nil
+	// 🎶 💃🕺
+	checkMap := make(map[interface{}]bool)
+	returnList := []interface{}{}
+	for _, interfaceValue := range i {
+		_, isThere := checkMap[interfaceValue]
+		if !isThere {
+			checkMap[interfaceValue] = true
+			returnList = append(returnList, interfaceValue)
+		}
+	}
+	return returnList
 }
--- assignment/assignment_test.go
+++ assignment/assignment_test.go
@@ -8,111 +8,136 @@ import (
 )
 
 func TestAddUint32(t *testing.T) {
-	/*
-		Sum uint32 numbers, return uint32 sum value and boolean overflow flag
-		cases need to pass:
-			math.MaxUint32, 1 => 0, true
-			1, 1 => 2, false
-			42, 2701 => 2743, false
-			42, math.MaxUint32 => 41, true
-			4294967290, 5 => 4294967295, false
-			4294967290, 6 => 0, true
-			4294967290, 10 => 4, true
-	*/
-	sum, overflow := AddUint32(math.MaxUint32, 1)
-
-	assert.Equal(t, uint32(0), sum)
-	assert.True(t, overflow)
+	tests := []struct {
+		name  string
+		x     uint32
+		y     uint32
+		want  uint32
+		want1 bool
+	}{
+		{"test1", math.MaxUint32, 1, 0, true},
+		{"test2", 1, 1, 2, false},
+		{"test3", 42, 2701, 2743, false},
+		{"test4", 42, math.MaxUint32, 41, true},
+		{"test5", 4294967290, 5, 4294967295, false},
+		{"test6", 4294967290, 6, 0, true},
+		{"test7", 4294967290, 10, 4, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := AddUint32(tt.x, tt.y)
+			assert.Equalf(t, tt.want, got, "AddUint32(%v, %v)", tt.x, tt.y)
+			assert.Equalf(t, tt.want1, got1, "AddUint32(%v, %v)", tt.x, tt.y)
+		})
+	}
 }
 
 func TestCeilNumber(t *testing.T) {
-	/*
-		Ceil the number within 0.25
-		cases need to pass:
-			42.42 => 42.50
-			42 => 42
-			42.01 => 42.25
-			42.24 => 42.25
-			42.25 => 42.25
-			42.26 => 42.50
-			42.55 => 42.75
-			42.75 => 42.75
-			42.76 => 43
-			42.99 => 43
-			43.13 => 43.25
-	*/
-	point := CeilNumber(42.42)
-
-	assert.Equal(t, 42.50, point)
+	tests := []struct {
+		name string
+		f    float64
+		want float64
+	}{
+		{"test1", 42.42, 42.50},
+		{"test2", 42, 42},
+		{"test3", 42.01, 42.25},
+		{"test4", 42.24, 42.25},
+		{"test5", 42.25, 42.25},
+		{"test6", 42.26, 42.50},
+		{"test7", 42.55, 42.75},
+		{"test8", 42.75, 42.75},
+		{"test9", 42.76, 43},
+		{"test10", 42.99, 43},
+		{"test11", 43.13, 43.25},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, CeilNumber(tt.f), "CeilNumber(%v)", tt.f)
+		})
+	}
 }
 
 func TestAlphabetSoup(t *testing.T) {
-	/*
-		String with the letters in alphabetical order.
-		cases need to pass:
-		 	"hello" => "ehllo"
-			"" => ""
-			"h" => "h"
-			"ab" => "ab"
-			"ba" => "ab"
-			"bac" => "abc"
-			"cba" => "abc"
-	*/
-	result := AlphabetSoup("hello")
-
-	assert.Equal(t, "ehllo", result)
+	tests := []struct {
+		name string
+		s    string
+		want string
+	}{
+		{"test1", "hello", "ehllo"},
+		{"test2", "", ""},
+		{"test3", "h", "h"},
+		{"test4", "ab", "ab"},
+		{"test5", "ba", "ab"},
+		{"test6", "bac", "abc"},
+		{"test7", "cba", "abc"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, AlphabetSoup(tt.s), "AlphabetSoup(%v)", tt.s)
+		})
+	}
 }
 
 func TestStringMask(t *testing.T) {
-	/*
-		Replace after n(uint) character of string with '*' character.
-		cases need to pass:
-			"!mysecret*", 2 => "!m********"
-			"", n(any positive number) => "*"
-			"a", 1 => "*"
-			"string", 0 => "******"
-			"string", 3 => "str***"
-			"string", 5 => "strin*"
-			"string", 6 => "******"
-			"string", 7(bigger than len of "string") => "******"
-			"s*r*n*", 3 => "s*r***"
-	*/
-	result := StringMask("!mysecret*", 2)
-
-	assert.Equal(t, "!m********", result)
+	tests := []struct {
+		name string
+		s    string
+		n    uint
+		want string
+	}{
+		{"test1", "!mysecret*", 2, "!m********"},
+		{"test2", "", 4, "*"},
+		{"test3", "a", 1, "*"},
+		{"test4", "string", 0, "******"},
+		{"test5", "string", 3, "str***"},
+		{"test6", "string", 5, "strin*"},
+		{"test7", "string", 6, "******"},
+		{"test8", "string", 7, "******"},
+		{"test9", "s*r*n*", 3, "s*r***"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, StringMask(tt.s, tt.n), "StringMask(%v, %v)", tt.s, tt.n)
+		})
+	}
 }
 
 func TestWordSplit(t *testing.T) {
 	words := "apple,bat,cat,goodbye,hello,yellow,why"
-	/*
-		Your goal is to determine if the first element in the array can be split into two words,
-		where both words exist in the dictionary(words variable) that is provided in the second element of array.
-
-		cases need to pass:
-			[2]string{"hellocat",words} => hello,cat
-			[2]string{"catbat",words} => cat,bat
-			[2]string{"yellowapple",words} => yellow,apple
-			[2]string{"",words} => not possible
-			[2]string{"notcat",words} => not possible
-			[2]string{"bootcamprocks!",words} => not possible
-	*/
-	result := WordSplit([2]string{"hellocat", words})
-
-	assert.Equal(t, "hello,cat", result)
+	tests := []struct {
+		name string
+		arr  [2]string
+		want string
+	}{
+		{"test1", [2]string{"hellocat", words}, "hello,cat"},
+		{"test2", [2]string{"catbat", words}, "cat,bat"},
+		{"test3", [2]string{"yellowapple", words}, "yellow,apple"},
+		{"test4", [2]string{"", words}, "not possible"},
+		{"test5", [2]string{"notcat", words}, "not possible"},
+		{"test6", [2]string{"bootcamprocks!", words}, "not possible"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, WordSplit(tt.arr), "WordSplit(%v)", tt.arr)
+		})
+	}
 }
 
 func TestVariadicSet(t *testing.T) {
-	/*
-		FINAL BOSS ALERT :)
-		Tip: Learn and apply golang variadic functions(search engine -> "golang variadic function" -> WOW You can really dance! )
-
-		Convert inputs to set(no duplicate element)
-		cases need to pass:
-			4,2,5,4,2,4 => []interface{4,2,5}
-			"bootcamp","rocks!","really","rocks! => []interface{"bootcamp","rocks!","really"}
-			1,uint32(1),"first",2,uint32(2),"second",1,uint32(2),"first" => []interface{1,uint32(1),"first",2,uint32(2),"second"}
-	*/
-	set := VariadicSet(4, 2, 5, 4, 2, 4)
-
-	assert.Equal(t, []interface{}{4, 2, 5}, set)
+	// FINAL BOSS ALERT :)
+	//	Tip: Learn and apply golang variadic functions(search engine -> "golang variadic function" -> WOW You can really dance! )
+	tests := []struct {
+		name string
+		i    []interface{}
+		want []interface{}
+	}{
+		{"test1", VariadicSet(4, 2, 5, 4, 2, 4), []interface{}{4, 2, 5}},
+		{"test2", VariadicSet("bootcamp", "rocks!", "really", "rocks!"), []interface{}{"bootcamp", "rocks!", "really"}},
+		{"test3", VariadicSet(1, uint32(1), "first", 2, uint32(2), "second", 1, uint32(2), "first"), []interface{}{1, uint32(1), "first", 2, uint32(2), "second"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, VariadicSet(tt.i...), "VariadicSet(%v)", tt.i...)
+		})
+	}
 }
